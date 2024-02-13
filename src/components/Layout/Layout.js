6d2ab73cd292c8493c72f7115dc83d1d764f0032
upendrabhanushali <u.bhanushali@zensar.com>
2024-02-13 15:11:46 +0530

    second commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from './Main'
+
+const Layout = () => {
+  return (
+    <>
+    <div>Layout</div>
+    
+    <Main/>
+    
+    </>
+  )
+}
+
+export default Layout
---
+++ src/components/Layout/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Main = () => {
+  return (
+    
+    <Outlet/>
+  )
+}
+
+export default Main
